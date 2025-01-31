045125d4aae8ef5cf9973d8c0738d68c88656b60
patrik.kristel <patrik.kristel@nexonera.com>
2025-01-31 14:12:32 +0100

    feat: add tf destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ module "my_vpc" {
 module "eks" {
   source         = "./modules/eks"
   cluster_name   = "selfhosted-sentry-eks"
-  k8s_version    = "1.27"
+  k8s_version    = "1.31"
   subnet_ids     = [module.subnets.private_eks_subnet_1a_id,module.subnets.private_eks_subnet_1b_id]
   node_count     = 1
   node_min       = 1
