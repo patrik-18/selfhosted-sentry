a9837b8e85de4bfd21162d8bfe891071baf1440b
meliovit-admin <dev@meliovit.com>
2025-02-02 13:29:02 +0100

    fix: correction aws-auth

--- terraform/modules/eks/eks.tf
+++ terraform/modules/eks/eks.tf
@@ -21,35 +21,3 @@ resource "aws_eks_node_group" "eks_nodes" {
     max_size     = var.node_max
   }
 }
-
-resource "kubernetes_config_map" "aws_auth" {
-  depends_on = [aws_eks_cluster.eks]
-
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles = jsonencode([
-      {
-        rolearn  = aws_iam_role.node_role.arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = ["system:bootstrappers", "system:nodes"]
-      },
-      {
-        rolearn  = var.iam_roles[0]
-        username = "admin"
-        groups   = ["system:masters"]
-      }
-    ])
-
-    mapUsers = jsonencode([
-      {
-        userarn  = var.iam_users[0]
-        username = "admin"
-        groups   = ["system:masters"]
-      }
-    ])
-  }
-}
